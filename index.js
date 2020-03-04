8854999a1fa88d4bda92e917827d71021d156a36
David Atherton <davidatherton92@gmail.com>
2020-03-04 08:45:08 +0000

    Connect to database

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const authRouter = require('./routes/auth');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 app = express();
+
+dotenv.config('.env');
 
 app.use('/api/user', authRouter);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
+
+mongoose.connect(
+	process.env.DB_CONNECT,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log('Connected to db')
+);
