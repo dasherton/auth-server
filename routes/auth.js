6edee19768da8986e2503cf2075ac6b5c1e4d551
David Atherton <davidatherton92@gmail.com>
2020-03-04 09:29:57 +0000

    Create user model instance

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,14 @@
 const router = require('Express').Router();
+const User = require('../models/user');
 
 router.post('/register', (request, response) => {
-	response.send('register');
+	const user = new User({
+		name: 'David',
+		email: 'My Email',
+		password: '12345'
+	});
+
+	response.send(user);
 });
 
 module.exports = router;
