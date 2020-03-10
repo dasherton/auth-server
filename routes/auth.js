d02b0dbd49f0ec69637a064f11ec526b917592e8
David Atherton <davidatherton92@gmail.com>
2020-03-10 09:39:07 +0000

    Validate register user request

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,26 @@
 const router = require('Express').Router();
 const User = require('../models/user');
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({ 
+	name: Joi.string()
+		.required(),
+	email: Joi.string()
+		.required()
+		.email(),
+	password: Joi.string()
+		.min(6)
+		.required()
+});
 
 router.post('/register', async (request, response) => {
+
+	const {error} = schema.validate(request.body);
+
+	if(error) {
+		return response.status(400).send(error.details[0].message);
+	}
+
 	const user = new User({
 		name: request.body.name,
 		email: request.body.email,
