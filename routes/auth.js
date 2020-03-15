7dd6d0c8eec5c893744b98559e9a17feef688b95
David Atherton <davidatherton92@gmail.com>
2020-03-15 15:30:51 +0000

    Hash password before saving

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('Express').Router();
 const User = require('../models/user');
 const { registerValidation } = require('../validation');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (request, response) => {
 
@@ -10,17 +11,20 @@ router.post('/register', async (request, response) => {
 		return response.status(400).send(error.details[0].message);
 	}
 
-	const user = new User({
-		name: request.body.name,
-		email: request.body.email,
-		password: request.body.password
-	});
-
 	const emailExists = await User.findOne({email: request.body.email});
-
+	
 	if(emailExists) {
 		return response.status(400).send('Email already exists');
 	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(request.body.password, salt);
+
+	const user = new User({
+		name: request.body.name,
+		email: request.body.email,
+		password: hashPassword
+	});
 
 	try {
 		const saved = await user.save();
