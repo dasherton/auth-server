0728433ea9fcf8d3f8d723a236307349ba3d1049
David Atherton <davidatherton92@gmail.com>
2020-04-28 14:49:53 +0100

    Verify correctness of auth-token

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,5 +3,12 @@ const jwt = require('jsonwebtoken');
 module.exports = function (request, response, next) {
 	const token = request.header('auth-token');
 	if (!token) return response.status(401).send('Access denied!');
-	next();
+
+	try{
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		request.user = verified;
+		next();
+	}catch (err) {
+		return response.status(400).send('Invalid token');
+	}
 }
